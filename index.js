9b6f7fe82a8b478ba27b1f65bf7ecb3d57ceb1c1
Ashley Miller <AshMilDevigner@gmail.com>
2017-01-23 13:51:58 -0500

    I updated your index.js.

--- index.js
+++ index.js
@@ -9,23 +9,25 @@ exports.debug = (message, status) => {
   const utcDate = dt.toUTCString();
   // Stores color to be available later on
   let color;
+  // Stores message
+  const msgcons = utcDate + ':\n   ' + chalk.black.bgGreen('[debugtool]') + ' ' + color(message);
   // DEBUG Switch if true it activates debbuging
-  if (process.env.DEBUG === 'true') {
+  if (process.env.DEBUG) {
     // Checks for status, if == success chalk is green
     if (status === 'success') {
       color = chalk.green;
+      // Console log the message with chalk included
+      console.log(msgcons);
       // Checks for status, if == error chalk is red
     } else if (status === 'error') {
       color = chalk.red;
+      // Console error the message with chalk included
+      console.error(msgcons);
       // Any other, chalk is yellow
     } else {
       color = chalk.yellow;
+      // Console warn the message with chalk included
+      console.warn(msgcons);
     }
-
-    // Stores message
-    const msgcons = utcDate + ':\n   ' + chalk.black.bgGreen('[debugtool]') + ' ' + color(message);
-
-    // Console log the message with chalk included
-    console.log(msgcons);
   }
-};
+}
